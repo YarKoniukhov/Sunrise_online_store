419c8a1577be3a04818332b3a323b7cc35c69878
YarKoniukhov <78385671+YarKoniukhov@users.noreply.github.com>
2023-02-05 23:37:54 +0200

    Update settings.py

--- myshop/myshop_main/settings.py
+++ myshop/myshop_main/settings.py
@@ -171,11 +171,4 @@ Configuration.configure(
 )
 
 
-"""EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'yarko1903@gmail.com'
-EMAIL_HOST_PASSWORD = 'hkripaxzoqrotjfq'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True"""
-
 
